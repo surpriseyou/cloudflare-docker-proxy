2033c82b72e06b49cfb2769951104d023d781d59
mty <surpriseyou@126.com>
2024-12-29 23:28:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.itcode.space": dockerHub,
+  "quay.itcode.space": "https://quay.io",
+  "gcr.itcode.space": "https://gcr.io",
+  "k8s-gcr.itcode.space": "https://k8s.gcr.io",
+  "k8s.itcode.space": "https://registry.k8s.io",
+  "ghcr.itcode.space": "https://ghcr.io",
+  "cloudsmith.itcode.space": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.itcode.space": dockerHub,
 };
 
 function routeByHosts(host) {
